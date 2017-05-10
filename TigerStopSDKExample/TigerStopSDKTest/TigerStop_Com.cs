3a7a5d45dd529b53e5941850d4ec2ab63224db98
TigerStop <opensource@tigerstop.com>
2017-05-10 07:40:05 -0700

    TigerStopSDK-Example: Fixed RetryCommand( ) loop that was caused by readBuffer never clearing the 'NACK' that called RetryCommand( ) in the first place.

--- TigerStopSDKExample/TigerStopSDKTest/TigerStop_Com.cs
+++ TigerStopSDKExample/TigerStopSDKTest/TigerStop_Com.cs
@@ -487,7 +487,7 @@ namespace TigerStopSDKExample
         private void HandleAck()
         {
             // If there isn't any kind of 'NACK' in the message, we have to treat it as a legitimate message.
-            if (!string.Join("", readBuffer.ToArray()).Contains("NACK"))
+            if (string.Join("", readBuffer.ToArray()).Contains("NACK"))
             {
                 if (!isScanning)
                 {
@@ -625,6 +625,9 @@ namespace TigerStopSDKExample
             }
             else
             {
+                // Clear the NACK from the buffer.
+                readBuffer.Clear();
+
                 RetryCommand();
             }
         }
